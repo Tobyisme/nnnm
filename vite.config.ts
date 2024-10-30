56b9e6b2d82dd72f2b5d3e7e2d000f15311a6a05
Tobyisme <ruib0204z@gmail.com>
2024-10-30 18:24:16 +0800

    d

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vuePlugin.default()],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
